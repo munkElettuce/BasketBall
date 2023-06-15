182e359fcda781564e257098e81e2ad30328558f
Dhanush S <111214391+munkElettuce@users.noreply.github.com>
2023-06-15 13:23:54 +0530

    added a BasketBall
    added a BasketBall and its correct parabola path to its basket

--- BasketBall.ino
+++ BasketBall.ino
@@ -1,7 +1,11 @@
-#include <BasketBall.h>
+
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+
+
+#include <BasketBall.h>
+#include <Background.h>
 
 #define OLED_WIDTH 128
 #define OLED_HEIGHT 64
@@ -9,30 +13,30 @@
 
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 
-
+int* temp;
 unsigned int basketCenterX,basketCenterY,basketRadius;
 
 void setup(){
   Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT);
+  BasketBall ball;
+  ball.begin(&display,2*BB_RADIUS,2*BB_RADIUS,BB_RADIUS);
+  
+
+
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
-  Playground basketBall;
-  basketBall.begin(&display);
-  basketBall.drawFilledCircle(2*BB_RADIUS,2*BB_RADIUS,BB_RADIUS);
-  unsigned int basketY=basketBall.setupPole(OLED_WIDTH-2*BB_RADIUS,2*BB_RADIUS,BB_RADIUS,10);  
-  //basketY is the Y co ordinate of basket
-  unsigned int basketX=OLED_WIDTH-(2*BB_RADIUS+4);//offset is set to 4 to separate basket from pole
-  //basketX is the X co ordinate of Basket
-  basketBall.drawThickLineY(basketX,basketY,2*BB_RADIUS,3);
 
-  basketRadius=2*BB_RADIUS;
-  basketCenterX=basketX-basketRadius;
-  basketCenterY=basketY;
-  basketBall.setBasket(basketCenterX,basketCenterY,basketRadius);
+
+  Playground basketBallGround;
+  temp=basketBallGround.setupBackground(BB_RADIUS,10,&display);
   
+  
+  ball.drawFilledCircle();
+  ball.drawPath(ball.ballPaths[0]);
   display.display();
-  // display.drawLine(2,2,12,12,WHITE);
+  Serial.begin(9600);
 }
 
 void loop(){
+  
 }
