dd336eb0a0e194c1250ffb00058ef7593f9079d2
Dhanush S <111214391+munkElettuce@users.noreply.github.com>
2023-06-22 12:04:02 +0530

    Update BasketBall.ino

--- BasketBall.ino
+++ BasketBall.ino
@@ -2,10 +2,11 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
-
-#include <BasketBall.h>
 #include <Background.h>
+#include <BasketBall.h>
+
+
+
 
 #define OLED_WIDTH 128
 #define OLED_HEIGHT 64
@@ -13,30 +14,45 @@
 
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 
-int* temp;
-unsigned int basketCenterX,basketCenterY,basketRadius;
 
+Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT);
+BasketBall ball;
+Playground basketBallGround;
 void setup(){
-  Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT);
-  BasketBall ball;
-  ball.begin(&display,2*BB_RADIUS,2*BB_RADIUS,BB_RADIUS);
+ 
   
+  
+  ball.begin(&display,2*BB_RADIUS,2*BB_RADIUS,BB_RADIUS);
 
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
 
 
-  Playground basketBallGround;
-  temp=basketBallGround.setupBackground(BB_RADIUS,10,&display);
   
   
-  ball.drawFilledCircle();
-  ball.drawPath(ball.ballPaths[0]);
-  display.display();
+  display.drawLine(64,48,64,64,WHITE);
+  
+  
+  // ball.drawPath(ball.ballPaths[0]);
+  // ball.tempDo();
+  for(int i=0;i<10;i++){
+
+    display.clearDisplay();
+    ball.drawFilledCircle();
+    basketBallGround.setupBackground(&display,BB_RADIUS,10);
+    ball.drawPath(ball.ballPaths[i%3]);
+    
+    display.display();
+    delay(1000);
+  }
+
+  bool isThrown=ball.shoot(1,116);
+  // display.display();
   Serial.begin(9600);
 }
 
 void loop(){
   
+   
 }
